2cff5cd1852dfb2fbb50b32fd1c81949ecaa6a9d
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-10 19:32:49 +0900

    react-firebase-hooksのuseAuthStateでログイン状態を条件分岐

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import SignIn from './components/SignIn';
+// トップ画面であるApp.jsでコンポーネントの出し分けを行う
+// ログインしていれば、Lineコンポーネントを出力する
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "./firebase"
+import Line from './components/Line';
 
 function App() {
+  // ログインしているかどうかの確認
+  const user = useAuthState(auth);
+  console.log("ログインしているかどうか", user);
+
   return (
     <div>
-      <SignIn />
+      {user ? <Line />: <SignIn />}
     </div>
   );
 }
