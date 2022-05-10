4898a0073d1b730969a2b4438e181a8a342e53fd
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-10 19:03:45 +0900

    auth認証機能の繋ぎ込み

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,18 @@
 // rfceでスニペット
 import { Button } from '@mui/material'
 import React from 'react'
+import firebase from "firebase/compat/app"
+import {auth} from "../firebase";
+
 
 function SignIn() {
   function signInWithGoogle(){
-
+    // google認証をするためにインスタンス化
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // ポップアップしてログインする
+    auth.signInWithPopup(provider);
   }
-  
+
   return (
     <div>
       <Button onClick={signInWithGoogle}>
