8b99b3c17edafb2769d90d8e9c7e4015f6adcfa6
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-11 22:01:19 +0900

    sendMessageコンポーネント作成

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import SignOut from './SignOut'
 import { db } from '../firebase'
+import SendMessage from './SendMessage';
 
 function Line() {
   // useStateの初期値は[]にする必要がある、
@@ -31,6 +32,7 @@ function Line() {
           </div>
         ))}
       </div>
+      <SendMessage />
     </div>
   )
 }
