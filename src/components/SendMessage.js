41d86b999c3159309aa9efcbe4f60605f5ee73f7
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-11 22:08:42 +0900

    inputの値をuseStateで格納する

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function SendMessage() {
+  const [message, setMessage] = useState("");
   return (
-    <div>SendMessage</div>
+    <div>
+      <form>
+        <div className='sendMsg'>
+          <input placeholder='メッセージを入力して下さい' type="text" onChange={(e) => {
+            setMessage(e.target.value);
+          }} /> 
+        </div>
+      </form>
+    </div>
   )
 }
 
