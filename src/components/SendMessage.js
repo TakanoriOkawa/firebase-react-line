e05a13656c38cf93c11da9fdd468923db6e9404b
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-11 22:19:00 +0900

    dbに保存する処理　まずはテキストのみを送信

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import React, {useState} from 'react'
+import { db } from '../firebase'
 
 function SendMessage() {
   const [message, setMessage] = useState("");
+
+  function SendMessage(e) {
+    // テキストをDBに送りたいだけで、画面をリロードする必要は無いので、
+    e.preventDefault(); // デフォルトの機能を止める
+
+    // dbに保存する
+    db.collection("message").add({
+      text: message,
+    })
+
+
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={SendMessage}>
         <div className='sendMsg'>
           <input placeholder='メッセージを入力して下さい' type="text" onChange={(e) => {
             setMessage(e.target.value);
